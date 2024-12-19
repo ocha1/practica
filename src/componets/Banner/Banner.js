fa5aa8064fd6c25d9184db1f5eeadf702c1572b0
Oscar Barragan <ing_oscar_barragan@hotmail.com>
2024-12-18 22:26:24 -0500

    Primeros pasos del proyecto

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>book Shop</h1>
+            <p>The best place to find you books</p>
+        </div>
+    </div>
+  )
+  
+};
+
+export default Banner;
